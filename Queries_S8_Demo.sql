15c044f90697e5c090ba6ad3bf5b55d4b81b8205
Gomolemo Mogau <gomolemomogau977@gmail.com>
2026-02-02 16:05:11 +0200

    Handling Max/Min filtering

--- Queries_S8_Demo.sql
+++ Queries_S8_Demo.sql
@@ -81,3 +81,62 @@ FROM (
 	FROM employee_details
 ) AS ts
 WHERE top_salary = 1;
+
+-- Min/Max value filtering allows you to filter data based on the lowest or highest values within each group
+-- 2. MIN / MAX value filtering
+
+USE maven_advanced_sql;
+
+CREATE TABLE sales(
+	id INT PRIMARY KEY,
+    sales_rep VARCHAR(50),
+    date DATE,
+    sales INT
+);
+
+INSERT INTO sales(id, sales_rep, date, sales) VALUES
+(1, 'Emma', '2024-08-01', 6),
+(2, 'Emma', '2024-08-02', 17),
+(3, 'Jack', '2024-08-02', 14),
+(4, 'Emma', '2024-08-04', 20),
+(5, 'Jack', '2024-08-05', 5),
+(6, 'Emma', '2024-08-07', 1);
+
+-- View the sales table
+SELECT * FROM sales;
+
+-- Goal: Return the most recent sales amount for each sale rep
+-- Return the most recent sales date for each sales reps 
+
+SELECT s.sales_rep, MAX(s.date) AS most_recent_date
+FROM sales s
+GROUP BY s.sales_rep;
+
+-- Return the most recent sales date for each sales rep + attempt to add on the sales
+SELECT s.sales_rep, MAX(s.date) AS most_recent_date, MAX(s.sales)
+FROM sales s
+GROUP BY s.sales_rep;
+
+-- Number of sales on most recent date: Group by + join approach
+WITH rd AS (
+    SELECT s.sales_rep, MAX(s.date) AS most_recent_date
+    FROM sales s
+    GROUP BY s.sales_rep
+)
+SELECT 
+    rd.sales_rep, 
+    rd.most_recent_date, 
+    s.date
+FROM rd
+LEFT JOIN sales s
+    ON rd.sales_rep = s.sales_rep
+   AND rd.most_recent_date = s.date;
+   
+-- Number of sales on most recent date: Window function approach
+SELECT * FROM
+(
+	SELECT sales_rep, date, sales,
+		ROW_NUMBER() OVER(PARTITION BY sales_rep ORDER BY date DESC) AS row_num
+	FROM sales
+) AS rn
+WHERE row_num = 1;
