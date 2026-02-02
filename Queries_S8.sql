16e549a9b969ab7d7b42fc472a193d623c23e2bb
Gomolemo Mogau <gomolemomogau977@gmail.com>
2026-02-02 20:39:58 +0200

    Handling Max/ Min filtering assignment

--- Queries_S8.sql
+++ Queries_S8.sql
@@ -25,3 +25,72 @@ SELECT id, student_name, grade_level, gpa, school_lunch, birthday, email
 FROM sc
 WHERE student_count = 1;
 
+-- ASSIGNMENT 2: Min/Max value filtering
+-- View the students and student grades tables
+USE maven_advanced_sql;
+
+SELECT * FROM students;
+SELECT * FROM student_grades;
+
+-- For each student, return the classes they took and their final grades
+SELECT 
+	s.id, s.student_name, sg.class_name, sg.final_grade
+FROM students s
+LEFT JOIN student_grades sg
+ON s.id = sg.student_id;
+
+-- Return each student's top grade and corresponding class:
+-- APPROACH 1: GROUP BY + JOIN
+
+SELECT 
+	s.id, s.student_name, MAX(sg.final_grade) AS top_grade
+FROM students s
+LEFT JOIN student_grades sg
+ON s.id = sg.student_id
+GROUP BY s.id, s.student_name;
+
+-- To avoid null values we must use a inner join
+SELECT 
+	s.id, s.student_name, MAX(sg.final_grade) AS top_grade
+FROM students s
+INNER JOIN student_grades sg
+ON s.id = sg.student_id
+GROUP BY s.id, s.student_name
+ORDER BY s.id;
+
+-- Final GROUP BY + JOIN query
+WITH tg AS (
+	SELECT 
+		s.id, s.student_name, MAX(sg.final_grade) AS top_grade
+	FROM students s
+	INNER JOIN student_grades sg
+	ON s.id = sg.student_id
+	GROUP BY s.id, s.student_name
+	ORDER BY s.id
+)
+SELECT 
+	tg.id, tg.student_name, tg.top_grade, sg.class_name
+FROM tg 
+LEFT JOIN student_grades sg
+ON tg.id = sg.student_id AND tg.top_grade = sg.final_grade;
+
+-- APPROACH 2: Window function
+-- Rank the student grades for each student
+SELECT 
+	s.id, s.student_name, sg.class_name, sg.final_grade,
+    DENSE_RANK() OVER(PARTITION BY s.student_name ORDER BY sg.final_grade DESC) AS grade_rank
+FROM students s
+LEFT JOIN student_grades sg
+ON s.id = sg.student_id;
+
+-- Final window function query
+SELECT * FROM
+(
+	SELECT 
+	s.id, s.student_name, sg.class_name, sg.final_grade,
+    DENSE_RANK() OVER(PARTITION BY s.student_name ORDER BY sg.final_grade DESC) AS grade_rank
+	FROM students s
+	LEFT JOIN student_grades sg
+	ON s.id = sg.student_id
+) AS gr
+WHERE grade_rank = 1;
