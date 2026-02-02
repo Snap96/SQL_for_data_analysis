8dfd613465233097d55993a0cef9f0cbe58f5aeb
Gomolemo Mogau <gomolemomogau977@gmail.com>
2026-02-02 21:26:49 +0200

    this is for handling piovting in sql with CASE statements

--- Queries_S8.sql
+++ Queries_S8.sql
@@ -94,3 +94,47 @@ SELECT * FROM
 	ON s.id = sg.student_id
 ) AS gr
 WHERE grade_rank = 1;
+
+-- ASSIGNMENT 3: Pivoting
+-- Combine the students and student grades tables
+ SELECT 
+	s.grade_level, sg.department, sg.final_grade
+FROM students s
+LEFT JOIN student_grades sg
+ON s.id = sg.student_id;
+
+-- Pivot the grade level column
+SELECT 
+	s.grade_level, sg.final_grade,
+    CASE WHEN s.grade_level = 9 THEN 1 ELSE 0 END AS freshman,
+    CASE WHEN s.grade_level = 10 THEN 1 ELSE 0 END AS sophomore,
+    CASE WHEN s.grade_level = 11 THEN 1 ELSE 0 END AS junior,
+    CASE WHEN s.grade_level = 12 THEN 1 ELSE 0 END AS senior
+FROM students s
+LEFT JOIN student_grades sg
+ON s.id = sg.student_id;
+
+-- Update the values to be final grades
+SELECT 
+	s.grade_level, 
+    CASE WHEN s.grade_level = 9 THEN sg.final_grade ELSE 0 END AS freshman,
+    CASE WHEN s.grade_level = 10 THEN sg.final_grade ELSE 0 END AS sophomore,
+    CASE WHEN s.grade_level = 11 THEN sg.final_grade ELSE 0 END AS junior,
+    CASE WHEN s.grade_level = 12 THEN sg.final_grade ELSE 0 END AS senior
+FROM students s
+LEFT JOIN student_grades sg
+ON s.id = sg.student_id;
+
+-- final summary table
+SELECT 
+	sg.department, 
+    ROUND(AVG(CASE WHEN s.grade_level = 9 THEN sg.final_grade END)) AS freshman,
+    ROUND(AVG(CASE WHEN s.grade_level = 10 THEN sg.final_grade END)) AS sophomore,
+    ROUND(AVG(CASE WHEN s.grade_level = 11 THEN sg.final_grade END)) AS junior,
+    ROUND(AVG(CASE WHEN s.grade_level = 12 THEN sg.final_grade END)) AS senior
+FROM students s
+LEFT JOIN student_grades sg
+ON s.id = sg.student_id
+WHERE sg.department IS NOT NULL
+GROUP BY sg.department
+ORDER BY sg.department;
--- Queries_S8_Demo.sql
+++ Queries_S8_Demo.sql
@@ -140,3 +140,43 @@ SELECT * FROM
 	FROM sales
 ) AS rn
 WHERE row_num = 1;
+
+-- PIVOTING
+-- Pivoting lets you transform rows into columns to summarize your data
+-- this can be achieved using CASE statements
+
+CREATE TABLE pizza_table (
+    category VARCHAR(50),
+    crust_type VARCHAR(50),
+    pizza_name VARCHAR(100),
+    price DECIMAL(5, 2)
+);
+
+INSERT INTO pizza_table (category, crust_type, pizza_name, price) VALUES
+    ('Chicken', 'Gluten-Free Crust', 'California Chicken', 21.75),
+    ('Chicken', 'Thin Crust', 'Chicken Pesto', 20.75),
+    ('Classic', 'Standard Crust', 'Greek', 21.50),
+    ('Classic', 'Standard Crust', 'Hawaiian', 19.50),
+    ('Classic', 'Standard Crust', 'Pepperoni', 18.75),
+    ('Supreme', 'Standard Crust', 'Spicy Italian', 22.75),
+    ('Veggie', 'Thin Crust', 'Five Cheese', 18.50),
+    ('Veggie', 'Thin Crust', 'Margherita', 19.50),
+    ('Veggie', 'Gluten-Free Crust', 'Garden Delight', 21.50);
+    
+-- View the pizza
+SELECT * FROM pizza_table;
+
+-- Create 1/0
+SELECT *,
+	CASE WHEN crust_type = 'Standard Crust' 	THEN 1 ELSE 0 END AS standard_crust,
+    CASE WHEN crust_type = 'Thin Crust' 		THEN 1 ELSE 0 END AS thin_crust,
+    CASE WHEN crust_type = 'Gluten-Free Crust'	THEN 1 ELSE 0 END AS glute_free_crust
+FROM pizza_table;
+
+-- Create a summary table of categories & pizza types
+SELECT pt.category,
+	SUM(CASE WHEN crust_type = 'Standard Crust' 	THEN 1 ELSE 0 END) AS standard_crust,
+    SUM(CASE WHEN crust_type = 'Thin Crust' 		THEN 1 ELSE 0 END) AS thin_crust,
+    SUM(CASE WHEN crust_type = 'Gluten-Free Crust'	THEN 1 ELSE 0 END) AS glute_free_crust
+FROM pizza_table pt
+GROUP BY pt.category;
